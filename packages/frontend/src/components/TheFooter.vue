05d637c9f50a54291c6a847ba0784cb41b5d096e
Mr.DoSS <artfut10@gmail.com>
2022-08-27 21:31:56 +0300

    Update footer

--- packages/frontend/src/components/TheFooter.vue
+++ packages/frontend/src/components/TheFooter.vue
@@ -5,15 +5,31 @@
         <div class="flex-grow-1 text-center mb-2 mb-lg-0">
           Happy Planet Club. All rights reserved.
         </div>
-        <div class="d-flex">
+        <div class="d-flex align-items-center">
           <div class="me-2">
             <a href="https://twitter.com/HappyPlanetNFT" target="_blank">
               <i class="bi bi-twitter"></i>
             </a>
           </div>
-          <div>
+          <div class="me-2">
+            <a
+              href="https://opensea.io/collection/happy-planet-club-genesis"
+              target="_blank"
+            >
+              <img src="/os.svg" />
+            </a>
+          </div>
+          <div class="me-2">
             <a href="https://discord.gg/dQqGuFuJvd" target="_blank">
               <i class="bi bi-discord"></i>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://etherscan.io/address/0xe8fab2124411e8f5643b15a4a439109ebdd7c9d5"
+              target="_blank"
+            >
+              <img src="/etherscan.svg" />
             </a>
           </div>
         </div>
